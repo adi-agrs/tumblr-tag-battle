dc0b6e73e2be6f1750d680b28d5bbc863dfb668d
Adi Agrawal <adityaagrawal9835@gmail.com>
2026-07-02 20:03:16 -0700

    removed misleading note totals

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from leaderboards import display_leaderboard
 tag1 = sys.argv[1]
 tag2 = sys.argv[2]
 
-top_posts_1, notes_1, likes_1, reblogs_1, replies_1 = analyze_tag(tag1)
-top_posts_2, notes_2, likes_2, reblogs_2, replies_2 = analyze_tag(tag2)
+top_posts_1, notes_1 = analyze_tag(tag1)
+top_posts_2, notes_2 = analyze_tag(tag2)
 
 if notes_1 > notes_2:
     print(f"\n🏆 Winner: #{tag1} with {notes_1} total notes!")

68008a8b7895357f466b27ed4cad5eb29d243dc7
Adi Agrawal <adityaagrawal9835@gmail.com>
2026-07-28 14:37:28 -0700

    fix app.py

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from analyze import analyze_tag
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "HOMEPAAAAAAAAAGE!"
+    return render_template('index.html')
 
-@app.route('/tags/<tag1>')
-def show_tag(tag1):
-    return f"Showing results for tag: {tag1}"
+@app.route('/compare', methods=['POST'])
+def compare():
+    tag1 = request.form['tag1']
+    tag2 = request.form['tag2']
 
-@app.route('/compare', methods=['POST', 'GET'])
-def compare_tags():
-    if request.method == 'POST':
-        tag1 = request.form['tag1']
-        tag2 = request.form['tag2']
-        top_posts_1, notes_1, likes_1, reblogs_1, replies_1 = analyze_tag(tag1)
-        top_posts_2, notes_2, likes_2, reblogs_2, replies_2 = analyze_tag(tag2)
+    top_posts_1, notes_1 = analyze_tag(tag1)
+    top_posts_2, notes_2 = analyze_tag(tag2)
 
-        if notes_1 > notes_2:
-            winner = f"🏆 Winner: #{tag1} with {notes_1} total notes!"
-        elif notes_2 > notes_1:
-            winner = f"🏆 Winner: #{tag2} with {notes_2} total notes!"
-        else:
-            winner = f"🤝 It's a tie! Both tags have {notes_1} notes."
-        return winner 
-    
-    # If GET request, render a simple form to input tags
-    return """
-    <form method="POST">
-            Tag 1: <input type="text" name="tag1"><br>
-            Tag 2: <input type="text" name="tag2"><br>
-            <input type="submit" value="Compare">
-        </form>
-    """
+    if notes_1 > notes_2:
+        winner = tag1
+    elif notes_2 > notes_1:
+        winner = tag2
+    else:
+        winner = None  # tie
 
+    return render_template(
+        'results.html',
+        tag1=tag1, tag2=tag2,
+        notes_1=notes_1, notes_2=notes_2,
+        top_posts_1=top_posts_1, top_posts_2=top_posts_2,
+        winner=winner
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)

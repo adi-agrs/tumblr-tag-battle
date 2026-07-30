a8a375c0427e220cdfe30ee478c6c4dbf8e8e2df
Adi Agrawal <adityaagrawal9835@gmail.com>
2026-07-29 19:10:15 -0700

    change fetch_limit to 100

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ api_secret = os.getenv("TUMBLR_API_SECRET")
 access_token = os.getenv("TUMBLR_ACCESS_TOKEN")
 access_secret = os.getenv("TUMBLR_ACCESS_SECRET")
 secret_key = os.getenv("SECRET_KEY")
-fetch_limit = 20 # maximum number of posts 
+fetch_limit = 100 # maximum number of posts 
 page_length = 20 # page length always needs to be smaller than max_posts
 leaderboard_limit = 1 # must be smaller than fetch_limit, otherwise it will return all posts
